e0dd41874f5445230a29379fed80b599931d1105
Sergey Zarouski <serge.zarouski@gmail.com>
2020-04-07 12:25:05 -0400

    Progress

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const dreams = [
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
+app.use('/images', require('serve-index')('public/images', {icons: true}));
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
